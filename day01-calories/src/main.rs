e9c913f1c40ddab57f3d6d323e902a717da958b8
andreasduerloo <andreasduerloo@gmail.com>
2022-12-01 17:27:43 +0100

    Removed match statement

--- day01-calories/src/main.rs
+++ day01-calories/src/main.rs
@@ -5,44 +5,38 @@ fn main() {
 
     let mut arguments = env::args();
 
-    match arguments.len() {
-        0 | 1 => {
+    if let Some(filename) = arguments.nth(1) {
+        if let Ok(content) = fs::read_to_string(filename) {
 
-            println!("No arguments were passed. Exiting. 🦌");
-            return
-
-        },
-        _ => {
-            let filename = arguments.nth(1);
-
-            if let Ok(content) = fs::read_to_string(filename.unwrap()) {
-
-                let input_vec: Vec<&str> = content.lines().collect();
+            let input_vec: Vec<&str> = content.lines().collect();
+    
+            let mut totals: Vec<usize> = Vec::new();
+            let mut current: usize = 0;
         
-                let mut totals: Vec<usize> = Vec::new();
-                let mut current: usize = 0;
-            
-                for line in input_vec {
-                    if line != "" {
-                        current += usize::from_str_radix(line, 10).unwrap();
-                    }
-                    else {
-                        let total: usize = current;
-                        totals.push(total);
-                        current = 0;
-                    }
+            for line in input_vec {
+                if line != "" {
+                    current += usize::from_str_radix(line, 10).unwrap();
                 }
-                totals.sort();
+                else {
+                    let total: usize = current;
+                    totals.push(total);
+                    current = 0;
+                }
+            }
+            totals.sort();
+    
+            println!("⭐ First star ⭐ - The highest amount of calories is {}.", totals[totals.len()-1]);
+            println!("🌟 Second star ✨ - The sum of the three highest calorie counts is {}.", totals[totals.len() - 1] + totals[totals.len() - 2] + totals[totals.len() - 3]);
         
-                println!("⭐ First star ⭐ - The highest amount of calories is {}.", totals[totals.len()-1]);
-                println!("🌟 Second star ✨ - The sum of the three highest calorie counts is {}.", totals[totals.len() - 1] + totals[totals.len() - 2] + totals[totals.len() - 3]);
-            
-            } else {
-        
-                println!("Could not read file. Exiting. 🦌");
-                return
-        
-            }            
+        } else {
+    
+            println!("Could not read file. Exiting. 🦌");
+            return
+    
         }
+    } else {
+
+        println!("No argument was passed. Exiting. 🦌");
+
     }
 }
