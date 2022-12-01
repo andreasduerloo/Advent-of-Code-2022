0567f943016a84895aaf43de0f43a2c9a0837ec5
andreasduerloo <andreasduerloo@gmail.com>
2022-12-01 16:01:45 +0100

    Clean up day 1

--- day01-calories/src/main.rs
+++ day01-calories/src/main.rs
@@ -1,26 +1,48 @@
 use std::fs;
+use std::env;
 
 fn main() {
-    let input = fs::read_to_string("input.txt").expect("Could not read file");
-    let input_vec: Vec<&str> = input.lines().collect();
 
-    let mut totals: Vec<usize> = Vec::new();
-    let mut current: usize = 0;
+    let mut arguments = env::args();
 
-    for line in input_vec {
-        if line != "" {
-            current += usize::from_str_radix(line, 10).unwrap();
-        }
-        else {
-            let total: usize = current;
-            totals.push(total);
-            current = 0;
+    match arguments.len() {
+        0 | 1 => {
+
+            println!("No arguments were passed. Exiting. 🦌");
+            return
+
+        },
+        _ => {
+            let filename = arguments.nth(1);
+
+            if let Ok(content) = fs::read_to_string(filename.unwrap()) {
+
+                let input_vec: Vec<&str> = content.lines().collect();
+        
+                let mut totals: Vec<usize> = Vec::new();
+                let mut current: usize = 0;
+            
+                for line in input_vec {
+                    if line != "" {
+                        current += usize::from_str_radix(line, 10).unwrap();
+                    }
+                    else {
+                        let total: usize = current;
+                        totals.push(total);
+                        current = 0;
+                    }
+                }
+                totals.sort();
+        
+                println!("⭐ First star ⭐ - The highest amount of calories is {}.", totals[totals.len()-1]);
+                println!("🌟 Second star ✨ - The sum of the three highest calorie counts is {}.", totals[totals.len() - 1] + totals[totals.len() - 2] + totals[totals.len() - 3]);
+            
+            } else {
+        
+                println!("Could not read file. Exiting. 🦌");
+                return
+        
+            }            
         }
     }
-
-    totals.sort();
-
-    println!("First star: the highest amount of calories is {}.", totals[totals.len()-1]);
-    println!("Second star: the sum of the three highest calorie counts is {}.", totals[totals.len() - 1] + totals[totals.len() - 2] + totals[totals.len() - 3]);
-
 }
