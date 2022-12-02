76ec21179aa7fd1d5c3f361a0a705a519129fa59
andreasduerloo <andreasduerloo@gmail.com>
2022-12-02 21:26:20 +0100

    Day 2

---
+++ day02-rockpaperscissors/src/main.rs
@@ -0,0 +1,59 @@
+use std::fs;
+use std::env;
+use std::collections::HashMap;
+
+fn main() {
+    let ruleset1: HashMap<&str, HashMap<&str, usize>> = HashMap::from([
+        ("X", HashMap::from([("A", 4), ("B", 1), ("C", 7)])),
+        ("Y", HashMap::from([("A", 8), ("B", 5), ("C", 2)])),
+        ("Z", HashMap::from([("A", 3), ("B", 9), ("C", 6)]))
+    ]);
+
+    let ruleset2: HashMap<&str, HashMap<&str, usize>> = HashMap::from([
+        ("X", HashMap::from([("A", 3), ("B", 1), ("C", 2)])),
+        ("Y", HashMap::from([("A", 4), ("B", 5), ("C", 6)])),
+        ("Z", HashMap::from([("A", 8), ("B", 9), ("C", 7)]))
+    ]);
+
+    let mut arguments = env::args();
+
+    if let Some(filename) = arguments.nth(1) {
+        if let Ok(content) = fs::read_to_string(filename) {
+
+            let mut input_vec: Vec<&str> = content.lines().collect();
+
+            let mut first_score: usize = 0;
+            let mut second_score: usize = 0;
+
+            while let Some(combination) = input_vec.pop() {
+                if let Some(score) = solve(combination, &ruleset1) {
+                    first_score += score;
+                }
+                if let Some(score) = solve(combination, &ruleset2) {
+                    second_score += score;
+                }
+            }
+
+            println!("⭐ First star ⭐ - Total score is {}.", first_score);
+            println!("🌟 Second star ✨ - Total score is {}.", second_score);
+        } else {
+            eprintln!("Could not read file. Exiting. 🦌");
+        }
+    } else {
+        eprintln!("No argument was passed. Exiting. 🦌");
+    }
+}
+
+fn solve(input: &str, ruleset: &HashMap<&str, HashMap<&str, usize>>) -> Option<usize> {
+    let moves: Vec<&str> = input.split_whitespace().collect();
+
+    if let Some(rule) = ruleset.get(moves[1]) {
+        if let Some(value) = rule.get(moves[0]) {
+            Some(*value)
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
