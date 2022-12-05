f3655afb3854098a0eac12adcd3188fe337ab08a
andreasduerloo <andreasduerloo@gmail.com>
2022-12-05 20:31:51 +0100

    Closure

--- day01-calories/src/main.rs
+++ day01-calories/src/main.rs
@@ -12,16 +12,16 @@ fn main() {
             let mut totals: Vec<usize> = Vec::new();
             let mut current: usize = 0;
         
-            for line in input_vec {
-                if line != "" {
+            input_vec.iter().for_each( |line| {
+                if *line != "" {
                     current += usize::from_str_radix(line, 10).unwrap();
-                }
-                else {
+                } else {
                     let total: usize = current;
                     totals.push(total);
                     current = 0;
                 }
-            }
+            });
+
             totals.sort();
     
             println!("⭐ First star ⭐ - The highest amount of calories is {}.", totals[totals.len()-1]);
