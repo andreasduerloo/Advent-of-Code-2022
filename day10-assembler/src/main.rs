ae4476ce86b35fd116f5df2ec8abcdde39b702a3
andreasduerloo <andreasduerloo@gmail.com>
2022-12-10 08:09:54 +0100

    Set up day 10

---
+++ day10-assembler/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+use std::env;
+
+fn main() {
+    let mut arguments = env::args();
+
+    if let Some(filename) = arguments.nth(1) {
+        if let Ok(input) = fs::read_to_string(filename) {
+            //
+        } else {
+            eprintln!("Could not read file. Exiting. 🦌");
+        }
+    } else {
+        eprintln!("No argument was passed. Exiting. 🦌");
+    }
+}
