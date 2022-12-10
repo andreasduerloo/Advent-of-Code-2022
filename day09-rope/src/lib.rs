34faac504f6e93154c8bee664e42da80b9745627
andreasduerloo <andreasduerloo@gmail.com>
2022-12-10 19:15:52 +0100

    Cleaned up day 9

--- day09-rope/src/lib.rs
+++ day09-rope/src/lib.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 pub type Point = (isize, isize);
 pub type Map = HashMap<Point, bool>;
 
+// enum direction?
+
 pub fn parse(instruction: &str) -> (&str, isize) {
     let pair: Vec<&str> = instruction.split_whitespace().collect();
     let direction: &str = pair[0];
@@ -11,172 +13,71 @@ pub fn parse(instruction: &str) -> (&str, isize) {
     (direction, distance)    
 }
 
-pub fn move_head(instruction: (&str, isize), head: &mut Point, tail: &mut Point, map: &mut Map) {
-    match instruction.0 {
-        "D" => {
-            for _i in 0..instruction.1 {
-                head.1 -= 1;
-                move_tail(&(head.0, head.1), tail, map);
-            }
-        },
-        "L" => {
-            for _i in 0..instruction.1 {
-                head.0 -= 1;
-                move_tail(&(head.0, head.1), tail, map);
-            }
-        },
-        "R" => {
-            for _i in 0..instruction.1 {
-                head.0 += 1;
-                move_tail(&(head.0, head.1), tail, map);
-            }
-        },
-        "U" => {
-            for _i in 0..instruction.1 {
-                head.1 += 1;
-                move_tail(&(head.0, head.1), tail, map);
-            }
-        },
-        _ => {
-            return
-        }
-    }
-}
-
-fn move_tail(head: &Point, tail: &mut Point, map: &mut Map) {
-    // Decide if we need to move - either dimension is two higher or lower
-    if (head.0 - tail.0) > 1 || (head.0 - tail.0) < (-1) || (head.1 - tail.1) > 1 || (head.1 - tail.1) < (-1) {
-        // Move the tail
-        // Horizontally
-        if head.1 == tail.1 {
-            if head.0 < tail.0 { // The head is to the left
-                tail.0 -= 1;
-            } else {
-                tail.0 += 1;
-            }
-        }
-        // Vertically
-        else if head.0 == tail.0 {
-            if head.1 < tail.1 { // The head is below the tail
-                tail.1 -= 1;
-            } else {
-                tail.1 += 1;
-            }
-        }
-        // Diagonally
-        else {
-            if head.0 > tail.0 && head.1 > tail.1 { // Top right
-                tail.0 += 1;
-                tail.1 += 1;
-            }
-            else if head.0 > tail.0 && head.1 < tail.1 { // Bottom right
-                tail.0 += 1;
-                tail.1 -= 1;
-            }
-            else if head.0 < tail.0 && head.1 > tail.1 { // Top left
-                tail.0 -= 1;
-                tail.1 += 1;
-            }
-            else { // Bottom left
-                tail.0 -= 1;
-                tail.1 -= 1;
-            }
-        }
-    }
-    // Mark current position
-    map.entry(*tail).or_insert(true);
-}
-
-pub fn move_head2(instruction: (&str, isize), rope: &mut [Point; 10], map: &mut Map) {
-    match instruction.0 {
-        "D" => {
-            for _i in 0..instruction.1 {
+pub fn move_rope(instruction: (&str, isize), rope: &mut [Point], map: &mut Map) {
+    for _i in 0..instruction.1 {
+        match instruction.0 {
+            "D" => {
                 rope[0].1 -= 1;
-                for j in 1..10 {
-                    move_tail2(rope, j);
-                    if j == 9 {
-                        map.entry(rope[9]).or_insert(true);
-                    }
-                }
-            }
-        },
-        "L" => {
-            for _i in 0..instruction.1 {
+            },
+            "L" => {
                 rope[0].0 -= 1;
-                for j in 1..10 {
-                    move_tail2(rope, j);
-                    if j == 9 {
-                        map.entry(rope[9]).or_insert(true);
-                    }
-                }
-            }
-        },
-        "R" => {
-            for _i in 0..instruction.1 {
+            },
+            "R" => {
                 rope[0].0 += 1;
-                for j in 1..10 {
-                    move_tail2(rope, j);
-                    if j == 9 {
-                        map.entry(rope[9]).or_insert(true);
-                    }
-                }
-            }
-        },
-        "U" => {
-            for _i in 0..instruction.1 {
+            },
+            "U" => {
                 rope[0].1 += 1;
-                for j in 1..10 {
-                    move_tail2(rope, j);
-                    if j == 9 {
-                        map.entry(rope[9]).or_insert(true);
-                    }
-                }
-            }
-        },
-        _ => {
-            return
+            },
+            _ => return
         }
+        follow_head(rope, map);
     }
 }
 
-fn move_tail2(rope: &mut [Point; 10], index: usize) {
-    // Decide if we need to move - either dimension is two higher or lower
-    if (rope[index - 1].0 - rope[index].0) > 1 || (rope[index - 1].0 - rope[index].0) < (-1) || (rope[index - 1].1 - rope[index].1) > 1 || (rope[index - 1].1 - rope[index].1) < (-1) {
-        // Move the tail
-        // Horizontally
-        if rope[index - 1].1 == rope[index].1 {
-            if rope[index - 1].0 < rope[index].0 { // The head is to the left
-                rope[index].0 -= 1;
-            } else {
-                rope[index].0 += 1;
+fn follow_head(rope: &mut [Point], map: &mut Map) {
+    for i in 1..rope.len() { // For each knot, one after the other
+        if (rope[i - 1].0 - rope[i].0) > 1 || (rope[i - 1].0 - rope[i].0) < (-1) || (rope[i - 1].1 - rope[i].1) > 1 || (rope[i - 1].1 - rope[i].1) < (-1) {
+            // We have to move this particular knot
+            // Horizontally
+            if rope[i - 1].1 == rope[i].1 {
+                if rope[i - 1].0 < rope[i].0 { // The previous knot is to the left
+                    rope[i].0 -= 1;
+                } else {
+                    rope[i].0 += 1;
+                }
             }
+            // Vertically
+            else if rope[i - 1].0 == rope[i].0 {
+                if rope[i - 1].1 < rope[i].1 { // The head is below the tail
+                    rope[i].1 -= 1;
+                } else {
+                    rope[i].1 += 1;
+                }
+            }
+            else {
+                if rope[i - 1].0 > rope[i].0 && rope[i - 1].1 > rope[i].1 { // Top right
+                    rope[i].0 += 1;
+                    rope[i].1 += 1;
+                }
+                else if rope[i - 1].0 > rope[i].0 && rope[i - 1].1 < rope[i].1 { // Bottom right
+                    rope[i].0 += 1;
+                    rope[i].1 -= 1;
+                }
+                else if rope[i - 1].0 < rope[i].0 && rope[i - 1].1 > rope[i].1 { // Top left
+                    rope[i].0 -= 1;
+                    rope[i].1 += 1;
+                }
+                else { // Bottom left
+                    rope[i].0 -= 1;
+                    rope[i].1 -= 1;
+               }
+            }
+            if i == rope.len() - 1 { // We just moved the tail
+                map.entry(rope[i]).or_insert(true);
+            }
+        } else { // The previous knot has not moved, we can break out of the entire loop - store the tail's location anyway to get the origin
+            map.entry(rope[rope.len() - 1]).or_insert(true);
+            return
         }
-        // Vertically
-        else if rope[index - 1].0 == rope[index].0 {
-            if rope[index - 1].1 < rope[index].1 { // The head is below the tail
-                rope[index].1 -= 1;
-            } else {
-                rope[index].1 += 1;
-            }
-        }
-        // Diagonally
-        else {
-            if rope[index - 1].0 > rope[index].0 && rope[index - 1].1 > rope[index].1 { // Top right
-                rope[index].0 += 1;
-                rope[index].1 += 1;
-            }
-            else if rope[index - 1].0 > rope[index].0 && rope[index - 1].1 < rope[index].1 { // Bottom right
-                rope[index].0 += 1;
-                rope[index].1 -= 1;
-            }
-            else if rope[index - 1].0 < rope[index].0 && rope[index - 1].1 > rope[index].1 { // Top left
-                rope[index].0 -= 1;
-                rope[index].1 += 1;
-            }
-            else { // Bottom left
-                rope[index].0 -= 1;
-                rope[index].1 -= 1;
-           }
-        }
-    }  
+    }
 }
--- day09-rope/src/main.rs
+++ day09-rope/src/main.rs
@@ -9,27 +9,21 @@ fn main() {
     if let Some(filename) = arguments.nth(1) {
         if let Ok(input) = fs::read_to_string(filename) {
             
-            let mut map: Map = HashMap::new();
+            let mut map1: Map = HashMap::new();
             let mut map2: Map = HashMap::new();
 
-            let mut head: Point = (0, 0);
-            let mut tail: Point = (0, 0);
-
+            let mut rope1 = [(0, 0); 2];
             let mut rope2 = [(0, 0); 10];
-
-            for i in 0..10 {
-                rope2[i] = (0, 0);
-            }
 
             input.lines()
                 .map(|s| parse(s))
                 .for_each(|inst| {
-                    move_head(inst, &mut head, &mut tail, &mut map);
-                    move_head2(inst, &mut rope2, &mut map2);
+                    move_rope(inst, &mut rope1, &mut map1);
+                    move_rope(inst, &mut rope2, &mut map2);
                 });
 
-            println!("Points passed: {}", map.len());
-            println!("Points passed: {}", map2.len());
+            println!("⭐ First star ⭐ - The tail has passed {} points.", map1.len());
+            println!("🌟 Second star ✨ - The tail has passed {} points.", map2.len());
 
         } else {
             eprintln!("Could not read file. Exiting. 🦌");
