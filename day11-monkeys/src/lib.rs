03be7eb3db699828dfc4a518816b800a5a53e60a
andreasduerloo <andreasduerloo@gmail.com>
2022-12-11 23:59:43 +0100

    Stuck for now - will come back

--- day11-monkeys/src/lib.rs
+++ day11-monkeys/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub struct Monkey<'a> {
     pub items: Vec<usize>,
     pub thrown: usize,
@@ -48,7 +50,7 @@ pub fn create_monkey(input: &str) -> Monkey {
     }
 }
 
-pub fn inspect_and_throw(monkeys: &mut Vec<Monkey>, index: usize) {
+pub fn inspect_and_throw(monkeys: &mut Vec<Monkey>, index: usize, star: usize) {
     match monkeys[index].items.len() {
         0 => return,
         _ => {
@@ -57,9 +59,37 @@ pub fn inspect_and_throw(monkeys: &mut Vec<Monkey>, index: usize) {
                 let mut local_item: usize = monkeys[index].items.pop().unwrap();
                 local_item = operation(&monkeys[index].rule, local_item);
                 
-                // Momentary relief
-                local_item = local_item / 3;
+                if star == 1 {
+                    // Momentary relief
+                    local_item = local_item / 3;
+                }
+               
+                // Test
+                if local_item % monkeys[index].div_by_test == 0 {
+                    let receiver: usize = monkeys[index].if_true;
+                    monkeys[receiver].items.push(local_item);
+                } else {
+                    let receiver: usize = monkeys[index].if_false;
+                    monkeys[receiver].items.push(local_item);
+                }
+                monkeys[index].thrown += 1;
+            }
+        }
+    }
+}
 
+pub fn inspect_and_throw_cached(monkeys: &mut Vec<Monkey>, index: usize, _star: usize, cache: &mut HashMap<usize, usize>) {
+    match monkeys[index].items.len() {
+        0 => return,
+        _ => {
+            for _i in 0..monkeys[index].items.len() {
+                // Inspect the item
+                let mut local_item: usize = monkeys[index].items.pop().unwrap();
+                local_item = operation(&monkeys[index].rule, local_item);
+                
+                // local_item = same_divisibility(local_item, cache, monkeys);
+                local_item = same_properties(local_item, cache, monkeys);
+                 
                 // Test
                 if local_item % monkeys[index].div_by_test == 0 {
                     let receiver: usize = monkeys[index].if_true;
@@ -93,3 +123,69 @@ pub fn operation(rule: &Vec<&str>, item: usize) -> usize {
     }
     0
 }
+
+// Does not work: for ex. 101 is not divisible by 2 or 3, so function would return 1, but 1 + 1 % 3 != 101 + 1 % 3
+// Looks like the remainders also play a role
+pub fn same_divisibility(input: usize, cache: &mut HashMap<usize, usize>, monkeys: &Vec<Monkey>) -> usize {
+    if let Some(known_value) = cache.get(&input) {
+        *known_value
+    } else {
+        let mut output: usize = 1;
+
+        for i in 0..monkeys.len() {
+            if input % monkeys[i].div_by_test == 0 {
+                output *= monkeys[i].div_by_test;
+            }
+        }
+    let _ = cache.insert(input, output);
+    output
+    }
+}
+
+// This one is far too slow
+pub fn same_properties(input: usize, cache: &mut HashMap<usize, usize>, monkeys: &Vec<Monkey>) -> usize {
+    // Store the remainders (where there is one) while building a number with the same divisibility
+    if let Some(known_value) = cache.get(&input) {
+        *known_value
+    } else {
+        let mut output: usize = 1;
+        let mut remainders: HashMap<usize, usize> = HashMap::new();
+
+        for i in 0..monkeys.len() {
+            if input % monkeys[i].div_by_test == 0 {
+                output *= monkeys[i].div_by_test;
+            } else {
+                remainders.insert(monkeys[i].div_by_test, input % monkeys[i].div_by_test);
+            }
+        }
+
+        if output == 1 { // Don't waste time trying all possible numbers
+            let _ = cache.insert(input, input);
+            return input;
+        }
+
+        // We now have a number that divides the way we like it, but the remainders are off
+        // Multiply it until it works out
+
+        let mut found: bool = false;
+        let mut attempt: usize = output;
+
+        while !found {
+            // Check if any remainders are off - in the worst case we should get the original input back, because it meets the conditions
+            let mut correct: usize = 0;
+            for key in remainders.keys() {
+                if attempt % key == *remainders.get(key).unwrap() {
+                    correct += 1;
+                }
+            }
+            if remainders.len() == correct {
+                found = true;
+            } else {
+                attempt += output;
+            }
+        }
+        let _ = cache.insert(input, attempt);
+        attempt
+    }
+}
+
--- day11-monkeys/src/main.rs
+++ day11-monkeys/src/main.rs
@@ -1,20 +1,38 @@
 use std::fs;
 use std::env;
 use day11_monkeys::*;
+use std::collections::HashMap;
 
 fn main() {
     let mut arguments = env::args();
 
     if let Some(filename) = arguments.nth(1) {
         if let Ok(input) = fs::read_to_string(filename) {
-            
+
             let mut monkeys: Vec<Monkey> = Vec::new(); // Array initialization needs us to implement copy for Vectors
 
             input.split("\r\n\r\n").for_each(|val| monkeys.push(create_monkey(val)));
 
-            for _i in 0..20 {
-                for j in 0..8 {
-                    inspect_and_throw(&mut monkeys, j);
+            let mut star = 1;
+            if let Some(value) = arguments.next() {
+                if value == "2" {
+                    star = 2;
+                }
+            }
+            
+            if star == 2 {
+                let mut divisibility_cache: HashMap<usize, usize> = HashMap::new();
+
+                    for _i in 0..10000 {
+                        for j in 0..8 {
+                            inspect_and_throw_cached(&mut monkeys, j, 2, &mut divisibility_cache);
+                        }
+                    }
+            } else {
+                for _i in 0..20 {
+                    for j in 0..8 {
+                        inspect_and_throw(&mut monkeys, j, 1);
+                    }
                 }
             }
 
@@ -25,8 +43,11 @@ fn main() {
 
             let monkey_business: usize = monkey_business_scores.iter().take(2).product();
 
-            println!("Score = {}", monkey_business);
-
+            if star == 2 {
+                println!("🌟 Second star ✨ - Score after 10,000 rounds: {}", monkey_business);
+            } else {
+                println!("⭐ First star ⭐ - Score after 20 rounds: {}", monkey_business);
+            }
         } else {
             eprintln!("Could not read file. Exiting. 🦌");
         }
