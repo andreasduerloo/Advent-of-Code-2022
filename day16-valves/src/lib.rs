4314c403ee63ad711a74eab1f1d789551ac0442c
andreasduerloo <andreasduerloo@gmail.com>
2022-12-25 21:56:17 +0100

    Some more  day 16

--- day16-valves/src/lib.rs
+++ day16-valves/src/lib.rs
@@ -8,9 +8,11 @@ struct Valve {
 // Other option: Valve contains the name of its neighbors, and we have a HashMap with the flow rates per valve.
 
 pub fn read_valve(line: &str) -> Valve {
-    //
+    let split_line: Vec<&str> = line.split_whitespace().collect();
 
     Valve {
-        //
+        name: split_line[1],
+        flow_rate: usize::from_str_radix(split_line[4][5..split_line[4].len() - 1], 10).unwrap();
+        // neighbors:
     }
 }
