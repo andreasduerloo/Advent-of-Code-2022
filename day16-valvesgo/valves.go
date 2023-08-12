44b980ecf9adfdcea0ca06ad0460a1623a72e030
Andreas Duerloo <andreasduerloo@gmail.com>
2023-08-12 19:47:10 +0200

    Still working on it

--- day16-valvesgo/valves.go
+++ day16-valvesgo/valves.go
@@ -3,13 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	var lines []string
-	input, err := os.ReadFile("./input.txt")
+	input, err := os.ReadFile("./exampleinput.txt")
 	if err != nil {
 		fmt.Println("Error:", err)
 	} else {
@@ -32,25 +33,28 @@ func main() {
 		}
 	}
 
-	for k, v := range valves {
-		fmt.Println(k, v)
-	}
-
 	// Set the initial state
 	start := state{
-		time:     0,
-		location: "AA",
-		moving:   false,
-		visited:  []string{"AA"},
+		location: valves["AA"],
+		open:     make([]string, 0),
 		pressure: 0,
-		parent:   nil,
+		ppm:      0,
+		parent:   "",
+		path:     make([]string, 0),
 	}
 
-	var statetree []*state
+	states := []*state{&start}
 
-	statetree = append(statetree, &start)
+	// Keep running over the states, creating a new slice with every pulse
+	for i := 1; i <= 30; i++ {
+		if i >= 25 { // PRUNE THE TREE
+			states = pulse(prune(states))
+		} else {
+			states = pulse(states)
+		}
+	}
 
-	// Keep running over the tree, creating a new one
+	fmt.Println("Highest:", highest(states))
 }
 
 type valve struct {
@@ -86,14 +90,81 @@ func newvalve(line string) *valve {
 }
 
 type state struct {
-	time     int
-	location string
-	moving   bool
-	visited  []string
+	location *valve
+	open     []string
 	pressure int
-	parent   *state
+	ppm      int
+	parent   string
+	path     []string
 }
 
 func pulse(states []*state) []*state {
-	//
+	out := make([]*state, 0)
+
+	for _, st := range states {
+		for _, n := range st.location.neighbors { // Move to all neighbors, except right back to the previous one without opening the valve
+			if st.parent != n.name {
+				newst := state{
+					location: n,
+					open:     st.open,
+					pressure: (st.pressure + st.ppm),
+					ppm:      st.ppm,
+					parent:   st.location.name,
+					path:     append(st.path, n.name),
+				}
+				out = append(out, &newst)
+			}
+		}
+		if st.location.flowrate > 0 && !slices.Contains(st.open, st.location.name) { // Is the valve closed and non-zero?
+			newst := state{ // Open it up
+				location: st.location,
+				open:     append(st.open, st.location.name),
+				pressure: (st.pressure + st.ppm),
+				ppm:      (st.ppm + st.location.flowrate),
+				parent:   st.location.name,
+				path:     append(st.path, st.location.name+"*"),
+			}
+
+			out = append(out, &newst)
+		}
+	}
+	return out
+}
+
+func highest(st []*state) int {
+	max := 0
+	open := make([]string, 0)
+	path := make([]string, 0)
+	ppm := 0
+
+	for _, s := range st {
+		if s.pressure >= max {
+			max = s.pressure
+			open = s.open
+			ppm = s.ppm
+			path = s.path
+		}
+	}
+
+	fmt.Println(open, ppm, path)
+	return max
+}
+
+func prune(st []*state) []*state {
+	maxppm := 0
+	out := make([]*state, 0)
+
+	for _, s := range st {
+		if s.ppm > maxppm {
+			maxppm = s.ppm
+		}
+	}
+
+	for _, s := range st {
+		if s.ppm >= maxppm/2 { // Anything more than twice as fast halfway through is not likely to be overtaken
+			out = append(out, s)
+		}
+	}
+
+	return out
 }
